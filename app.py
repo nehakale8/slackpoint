99a9f34f0719106c62934b7416eb40989729e7d1
Mithil Dani <danimithil@gmail.com>
2022-10-04 02:21:15 -0400

    add slack clients and slack events api

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from slack import WebClient
+from slackeventsapi import SlackEventAdapter
+from configuration.env_config import Config
+
 
 app = Flask(__name__)
+
+#instantiating slack client
+slack_client = WebClient(Config.SLACK_BOT_TOKEN)
+slack_events_adapter = SlackEventAdapter(
+    Config.SLACK_SIGNING_SECRET, "/slack/events", app
+)
 
 
 @app.route('/')
