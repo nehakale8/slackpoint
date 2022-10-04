f55c88566e26ded137a724dd351540ffbbbbf850
Neha <kaleneha997@gmail.com>
2022-10-04 16:58:38 -0400

    initial setup

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-
-
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:abc123@slackpoint_db/'
+db = SQLAlchemy(app)
+                                                
 @app.route('/')
 def basic():
     return 'Hello World'
