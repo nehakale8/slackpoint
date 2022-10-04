00870714b5a0442324c5ad1c6d5a584610c3534e
vanshmehta-7 <vanshm967@gmail.com>
2022-10-04 17:16:01 -0400

    update models.py

--- models.py
+++ models.py
@@ -5,34 +5,25 @@ class Task(db.Model):
 
     __tablename__ = 'task'
 
-    id = db.Column(db.Integer, primary_key = True)
+    task_id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     description = db.Column(db.Text())
     points = db.Column(db.Integer)
     deadline = db.Column(db.DateTime)
     created_on = db.Column(db.DateTime, default = datetime.now())
-    updated_on = db.Column(db.DateTime, default = datetime.now())
-
-
-    def __init__(self, description, points, deadline, created_on, updated_on):
-        self.description = description
-        self.points = points
-        self.deadline = deadline
-        self.created_on = created_on
-        self.updated_on = updated_on
-
+    updated_on = db.Column(db.DateTime)
 
 class Assignment(db.Model):
 
     __tablename__ = 'assignment'
 
-    id = db.Column(db.Integer, primary_key = True)
-    isDone = 
-    created_on = 
-    updated_on = 
+    assignment_id = db.Column(db.Integer, db.ForeignKey("task.task_id"))
+    progress = db.Column(db.Float)
+    created_on = db.Column(db.DateTime, db.ForeignKey("task.created_on"), default = datetime.now())
+    updated_on = db.Column(db.DateTime, db.ForeignKey("task.updated_on"))
 
+class User(db.Model):
 
-    def __init__(self, ):
-        self.isDone = isDone
-        self.created_on = created_on
-        self.updated_on = updated_on
+    __tablename__ = 'user'
 
+    user_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    user_points = db.Column(db.Integer, db.ForeignKey("task.points"))
