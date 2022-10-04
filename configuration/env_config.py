1f6e4e3a9b644fa2d5f95f03f0a26e35cefb1c73
Mithil Dani <danimithil@gmail.com>
2022-10-04 02:21:38 -0400

    add env config

---
+++ configuration/env_config.py
@@ -0,0 +1,14 @@
+import os
+
+
+class Config(object):
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_ECHO = True
+    # SLACK API key:
+    SLACK_SIGNING_SECRET = os.environ.get("SLACK_SIGNING_SECRET")
+    SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+    VERIFICATION_TOKEN = os.environ.get("VERIFICATION_TOKEN")
+
+
+
