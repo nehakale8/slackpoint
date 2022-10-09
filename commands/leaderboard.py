191144fa1982e37123cf45f8152735ae1c2e1670
Mithil Dani <danimithil@gmail.com>
2022-10-09 13:49:01 -0400

    added leaderboard test cases

--- commands/leaderboard.py
+++ commands/leaderboard.py
@@ -13,31 +13,26 @@ class Leaderboard:
         }
     }
 
-    def __init__(self, progress: float = 0.0):
-        self.progress = progress
+    def __init__(self):
         self.payload = {
             "response_type": "ephemeral",
             "blocks": []
         }
 
-    def view_leaderboard(self):
-        tasks = []
-
-        tasks_with_progress = Assignment.query.join(Task).join(User). \
+    def view_leaderboard(self, top_k: int = 5) -> dict:
+        top_5_leaderboard = Assignment.query.join(Task).join(User). \
             with_entities(User.user_id, User.slack_user_id, func.sum(Task.points).label("total_points")). \
             filter(Assignment.progress == 1). \
             group_by(User.user_id). \
-            order_by(desc("total_points"))[:5]
-        tasks.extend(tasks_with_progress)
-        print(tasks)
+            order_by(desc("total_points"))[:top_k]
+
         # parse them
-        print(tasks)
-        count=0
-        for task in tasks:
-            count+=1
+        count = 0
+        for user in top_5_leaderboard:
+            count += 1
             point = deepcopy(self.base_leaderboard_block_format)
-            point["text"]["text"] = point["text"]["text"].format(pos=count, userid=task.slack_user_id,
-                                                                 points=task.total_points)
+            point["text"]["text"] = point["text"]["text"].format(pos=count, userid=user.slack_user_id,
+                                                                 points=user.total_points)
             self.payload["blocks"].append(point)
         if not self.payload["blocks"]:
             self.payload["blocks"].append({
