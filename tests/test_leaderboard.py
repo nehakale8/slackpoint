bcbc463af8c48ac065eb01af97a62c49269a0214
Neha <kaleneha997@gmail.com>
2022-10-09 00:38:06 -0400

    code cov and partial leadership test

---
+++ tests/test_leaderboard.py
@@ -0,0 +1,72 @@
+from commands.leaderboard import Leaderboard
+from tests.mockmodels import mock_leaderboard_task_5, mock_leaderboard_task_6, mock_get_sqlalchemy
+
+def test_view_leadership_2tasks(
+    mock_leaderboard_task_5,
+    mock_leaderboard_task_6,
+    mock_get_sqlalchemy,
+):
+    # Mocking DB call
+    mock_get_sqlalchemy.join.return_value.join.return_value.\
+        with_entities.return_value.\
+        filter.return_value.\
+        group_by.return_value.\
+        order_by.return_value = [mock_leaderboard_task_5, mock_leaderboard_task_6]
+
+    # test function
+    lb = Leaderboard(progress=0.0)
+    payload = lb.view_leaderboard()
+
+    # expectation
+    expected_payload = {
+        'response_type': 'ephemeral',
+        'blocks': [
+            {
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': '1. <@{userid}> has 10 points!'
+                }
+            },
+            {
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': '2. <@{userid}> has 2 points!'
+                }
+            }
+        ]
+    }
+
+    assert payload == expected_payload
+
+
+def test_view_leadership_0tasks(
+    mock_get_sqlalchemy,
+):
+    # Mocking DB call
+    mock_get_sqlalchemy.join.return_value.join.return_value.\
+        with_entities.return_value.\
+        filter.return_value.\
+        group_by.return_value.\
+        order_by.return_value = []
+
+    # test function
+    lb = Leaderboard(progress=0.0)
+    payload = lb.view_leaderboard()
+
+    # expectation
+    expected_payload = {
+        'response_type': 'ephemeral',
+        'blocks': [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ">Looks like the competition hasn't started yet :("
+                }
+            }
+        ]
+    }
+
+    assert payload == expected_payload
