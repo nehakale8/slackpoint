1dd41e3f61dcd2b18a4851ff98c30d7fdde6bfc4
Neha <kaleneha997@gmail.com>
2022-10-09 02:16:57 -0400

    leadership test pt 3

--- tests/mockmodels.py
+++ tests/mockmodels.py
@@ -65,22 +65,34 @@ def mock_completed_task_4():
 
 @pytest.fixture
 def mock_leaderboard_task_5():
-    my_model = User.query.join(Task)(
+    my_model = Task(
         task_id=5,
         description="This is Task 4",
         points=18,
-        deadline="2022-08-26",
+        deadline="2022-08-26"
+    )
+    my_model2 = User(
         slack_user_id="nkale2"
     )
-    return my_model
+    my_model3 = Assignment(
+        user_id = my_model2,
+        assignment_id = my_model
+    )
+    return my_model3
 
 @pytest.fixture
 def mock_leaderboard_task_6():
-    my_model = User.query.join(Task)(
+    my_model = Task(
         task_id=6,
         description="This is Task 4",
         points=12,
-        deadline="2022-08-26",
+        deadline="2022-08-26"
+    )
+    my_model2 = User(
         slack_user_id="vpmehta"
     )
-    return my_model
+    my_model3 = Assignment(
+        user_id = my_model2,
+        assignment_id = my_model
+    )
+    return my_model3
